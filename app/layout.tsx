7533924be69542c72d0fbbae2a7dc13a1fe147b4
psychowm <psychomamba83@gmail.com>
2026-01-28 10:40:58 +0330

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,9 +26,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
+    <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+        suppressHydrationWarning={true}
+    >
         {children}
       </body>
     </html>
