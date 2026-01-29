12e959c50bd8d6a8daba76e4018a54aa9168899d
iliawm <psychomamba83@gmail.com>
2026-01-29 09:41:29 +0330

    profile section

--- Components/Profile.tsx
+++ Components/Profile.tsx
@@ -1,3 +1,4 @@
+"use client"
 import Image from "next/image";
 import { CiLocationOn } from "react-icons/ci";
 import { FaStar } from "react-icons/fa6";
@@ -10,7 +11,7 @@ export function Profile() {
         </div>
         <div className={"w-full h-9/10 font-bold flex justify-start  pl-20 max-h-270"}>
                 <div className={"leftside w-2/5 h-full pt-20 flex justify-center items-start "}>
-                    <div className="w-8/10 h-6/10 flex justify-start items-center rounded-lg shadow-2xl shadow-gray-300 p-5 right-10 max-w-70 max-h-102.5 min-h-87.5 min-w-55 relative flex-col">
+                    <div className="w-8/10 h-fit flex justify-start items-center rounded-lg shadow-2xl shadow-gray-300 p-5 right-10 max-w-70 max-h-102.5 min-h-87.5 min-w-55 relative flex-col">
                         <Image src={"/picture.png"} width={200} height={200} alt={""} className={"w-[100%] relative max-w-[150px]"}/>
                         <div className={"w-full h-full flex flex-col justify-start p-3 gap-5 pt-6"}>
                             <div className={"text-xl"}>Username</div>
@@ -19,18 +20,32 @@ export function Profile() {
                             </div>
                             <div className={"text-xl flex flex-row items-center gap-3  opacity-75"}>
                                 <div className={"text-yellow-500 text-2xl  flex items-center"}><FaStar /></div>
-                                <div className={"text-xl font-light "}> x / 5</div>
+                                <div className={"text-xl font-light flex"}> ? / 5</div>
                             </div>
 
                         </div>
                     </div>
                 </div>
                 <div className={"rightside w-3/5 h-full flex justify-center items-center"}>
-                    <div className={"w-full h-9/10 shadow-gray-300 shadow-xl rounded-2xl"}>
+                    <div className={"w-full h-fit shadow-gray-300 shadow-xl rounded-2xl"}>
                         <div className="top w-full h-1/10 flex p-5 text-2xl font-bold items-center ">
                             profile
                         </div>
-                        <div className="bot w-full h-9/10"></div>
+                        <div className="bot w-full h-9/10 flex p-5 py-0 text-sm font-medium flex-col items-start">
+                            <div className={"opacity-60 mb-4"}>your accounts info lives here</div>
+                            <div className={"opacity-60 mb-4"}>phone</div>
+                            <input type="number" className={"w-full h-10 border border-gray-300 rounded outline-0 focus:ring-0 mb-10"}/>
+                            <div className={"opacity-60 mb-4"}>Email</div>
+                            <input type="email" className={"w-full h-10 border border-gray-300 rounded outline-0 focus:ring-0 mb-10"}/>
+                            <div className={"opacity-60 mb-4"}>username</div>
+                            <input type="email" className={"w-full h-10 border border-gray-300 rounded outline-0 focus:ring-0 mb-10"}/>
+                            <input type="submit" className={"w-full h-10 border border-gray-300 rounded outline-0 focus:ring-0 mb-10 hover:bg-gray-400 transition-all duration-500 ease-linear"} onClick={()=>{
+
+                            }}/>
+
+
+
+                        </div>
                     </div>
                 </div>
         </div>
--- Components/dashboard.tsx
+++ Components/dashboard.tsx
@@ -8,49 +8,49 @@ import { CgMoreR } from "react-icons/cg";
 
 export function Dashboard({ id }: { id: string }) {
     return (
-        <div className={"w-[20%] h-[100%] flex flex-col bg-white absolute z-30 min-w-[170px] "}>
+        <div className={"w-[20%] h-full flex flex-col bg-white absolute z-30 min-w-42.5 "}>
             <div className="title h-[20%] w-full mb-5 text-2xl text-indigo-700 flex items-center justify-center">
                 Dashboard
             </div>
             <div className="custom-scrollbar w-full h-75/100 text-xl items-start flex flex-col justify-start p-1 gap-1 bg-white overflow-y-scroll overflow-x-hidden">
                 <div className="owned mt-2 text-base opacity-50">Private</div>
-                <Link href={`/users/${id}?section=profile`} className="w-full h-8/100 opacity-95 gap-3 flex items-center justify-start min-h-[33px] hover:bg-gray-400 transition-all duration-[1s] ease-linear">
+                <Link href={`/users/${id}?section=profile`} className="w-full h-8/100 opacity-95 gap-3 flex items-center justify-start min-h-8.25 hover:bg-gray-400 transition-all duration-1000 ease-linear">
                     <VscAccount className={"text-lg w-10"}/>
                     <div className="w-full flex items-cente justify-start">Profile</div>
                 </Link>
-                <Link href={`/users/${id}?section=security`} className="w-full h-8/100 opacity-95 gap-3 flex items-center justify-start min-h-[33px] hover:bg-gray-400 transition-all duration-[1s] ease-linear">
+                <Link href={`/users/${id}?section=security`} className="w-full h-8/100 opacity-95 gap-3 flex items-center justify-start min-h-8.25 hover:bg-gray-400 transition-all duration-1000 ease-linear">
                     <IoLockOpenOutline className={"text-lg w-10"}/>
                     <div className="w-full flex items-cente justify-start">security</div>
                 </Link>
-                <Link href={`/users/${id}?section=other`} className="w-full h-8/100 opacity-95 gap-3 flex items-center justify-start min-h-[33px] hover:bg-gray-400 transition-all duration-[1s] ease-linear">
+                <Link href={`/users/${id}?section=other`} className="w-full h-8/100 opacity-95 gap-3 flex items-center justify-start min-h-8.25 hover:bg-gray-400 transition-all duration-1000 ease-linear">
                     <TiFolderDelete className={"text-lg w-10"}/>
                     <div className="w-full flex items-cente justify-start">Other</div>
                 </Link>
                 {/*devider*/}
                 <div className="owned mt-2 text-base opacity-50">Owned</div>
-                <Link href={`/users/${id}?section=courses`} className="w-full h-8/100 opacity-95 gap-3 flex items-center justify-start min-h-[33px] hover:bg-gray-400 transition-all duration-[1s] ease-linear">
+                <Link href={`/users/${id}?section=courses`} className="w-full h-8/100 opacity-95 gap-3 flex items-center justify-start min-h-8.25 hover:bg-gray-400 transition-all duration-1000 ease-linear">
                     <PiChalkboardTeacherLight className={"text-lg w-10"}/>
                     <div className="w-full flex items-cente justify-start">Courses</div>
                 </Link>
-                <Link href={`/users/${id}?section=videos`} className="w-full h-8/100 opacity-95 gap-3 flex items-center justify-start min-h-[33px] hover:bg-gray-400 transition-all duration-[1s] ease-linear">
+                <Link href={`/users/${id}?section=videos`} className="w-full h-8/100 opacity-95 gap-3 flex items-center justify-start min-h-8.25 hover:bg-gray-400 transition-all duration-1000 ease-linear">
                     <MdVideoSettings className={"text-lg w-10"}/>
                     <div className="w-full flex items-cente justify-start">Videos</div>
                 </Link>
-                <Link href={`/users/${id}?section=books`} className="w-full h-8/100 opacity-95 gap-3 flex items-center justify-start min-h-[33px] hover:bg-gray-400 transition-all duration-[1s] ease-linear">
+                <Link href={`/users/${id}?section=books`} className="w-full h-8/100 opacity-95 gap-3 flex items-center justify-start min-h-8.25 hover:bg-gray-400 transition-all duration-1000 ease-linear">
                     <PiBooks className={"text-lg w-10"}/>
                     <div className="w-full flex items-cente justify-start">Books</div>
                 </Link>
-                <Link href={`/users/${id}?section=status`} className="w-full h-8/100 opacity-95 gap-3 flex items-center justify-start min-h-[33px] hover:bg-gray-400 transition-all duration-[1s] ease-linear">
+                <Link href={`/users/${id}?section=status`} className="w-full h-8/100 opacity-95 gap-3 flex items-center justify-start min-h-8.25 hover:bg-gray-400 transition-all duration-1000 ease-linear">
                     <MdOutlineMedicalInformation className={"text-lg w-10"}/>
                     <div className="w-full flex items-cente justify-start">Status</div>
                 </Link>
                 {/*devider*/}
                 <div className="owned mt-2 text-base opacity-50">Advanced</div>
-                <Link href={`/users/${id}?section=support`} className="w-full h-8/100 opacity-95 gap-3 flex items-center justify-start min-h-[33px] hover:bg-green-500 transition-all duration-[1s] ease-linear">
+                <Link href={`/users/${id}?section=support`} className="w-full h-8/100 opacity-95 gap-3 flex items-center justify-start min-h-8.25 hover:bg-green-500 transition-all duration-1000 ease-linear">
                     <MdSupportAgent className={"text-lg w-10"}/>
                     <div className="w-full flex items-cente justify-start">Support</div>
                 </Link>
-                <Link href={`/users/${id}?section=more`} className="w-full h-8/100 opacity-95 gap-3 flex items-center justify-start min-h-[33px] hover:bg-red-600 transition-all duration-[1s] ease-linear">
+                <Link href={`/users/${id}?section=more`} className="w-full h-8/100 opacity-95 gap-3 flex items-center justify-start min-h-8.25 hover:bg-red-600 transition-all duration-1000 ease-linear">
                     <CgMoreR className={"text-lg w-10"}/>
                     <div className="w-full flex items-cente justify-start">More</div>
                 </Link>
