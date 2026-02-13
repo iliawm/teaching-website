d4628c60edcbfca7f423403da73e7e899723c7bf
iliawm <psychomamba83@gmail.com>
2026-02-13 23:25:10 +0330

    auth-client doesnt accept public shared users fix

--- lib/(auth)/auth-cient.ts
+++ lib/(auth)/auth-cient.ts
@@ -1,10 +1,10 @@
 import {createAuthClient} from "better-auth/react";
 
-const baseURL = process.env.NEXT_PUBLIC_VERCEL_URL
-    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+const baseURL = typeof window !== 'undefined'
+    ? window.location.origin
     : 'http://localhost:3000';
 
 export const authClient = createAuthClient({
     baseURL,
 });
-export const {useSession , signIn , signOut , signUp } = authClient
+export const {useSession, signIn, signOut, signUp} = authClient
