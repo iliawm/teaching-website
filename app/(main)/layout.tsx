40f15890bc60cbece3a275ee5eca9eb057634ef2
iliawm <psychomamba83@gmail.com>
2026-02-21 21:44:54 +0330

    post role assignment for vercel to accept deployment

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -11,6 +11,7 @@ export default function RootLayout({
 }>) {
   return (
    <>
+       <Analytics/>
         {children}
    </>
   );
--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server'
 import Product from '@/models/product'
 import mongoose from 'mongoose'
+import {getSession} from "@/lib/(auth)/auth";
 
 await mongoose.connect(process.env.MONGODB_URI!)
 
@@ -10,6 +11,10 @@ export async function GET() {
 }
 
 export async function POST(request: NextRequest) {
+    const session = await getSession()
+    if (!session?.user || session.user.email !== 'iliabayat83@gmail.com') {
+        return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
+    }
     const body = await request.json()
     const product = await Product.create(body)
     return NextResponse.json(product)
