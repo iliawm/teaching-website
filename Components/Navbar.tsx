b4a8b53430a00cc66f0be7cc19e406f5a67a7da7
psychowm <psychomamba83@gmail.com>
2026-02-27 03:22:47 +0330

    DESIGNED SEARCH ANIMATION AND ITS CONDITIONAL RENDERING

--- Components/Navbar.tsx
+++ Components/Navbar.tsx
@@ -14,14 +14,16 @@ export default function Navbar() {
     const logged_in = session?.user
     const [open,setOpen]=useState(false)
     const router = useRouter()
-    const [search,setsearh]=useState(false)
+    const [search,setsearch]=useState(false)
     return (
         <div className="w-full  flex justify-center mb-10 relative" >
         <div className="bg-white w-full flex justify-between items-center h-15.5 rounded-full md:px-7 opacity-60 px-3">
-            <div className="w-full top-r h-full flex items-center justify-between " >
-                <div className={"flex items-center justify-start gap-6"}>
+            <div className={`w-full top-r h-full md:flex items-center justify-between  ${search?"hidden":"flex"}`} >
+
+                <div className={`flex items-center justify-start gap-6 ${search ? "hidden md:flex" : "flex" }`}>
+
                 {logged_in?
-                    <Link href={`dashboard?section=profile`} className="acc w-12 h-12 text-2xl flex justify-center items-center bg-indigo-500 rounded-full text-white">
+                    <Link href={`dashboard?section=profile`} className={`acc  w-12 h-12 text-2xl flex justify-center items-center bg-indigo-500 rounded-full text-white`}>
                     {`${logged_in.name[0]}`.toUpperCase()}
                 </Link> :
                     <Link href={"/auth/signin"} className="acc lg:w-15 lg:text-4xl flex justify-center text-3xl w-fit">
@@ -40,6 +42,7 @@ export default function Navbar() {
                         }>
                     <VscBell />
                 </button>
+                    
                 </div>
                 
                
@@ -50,7 +53,15 @@ export default function Navbar() {
 
                 {/*</Link>*/}
             </div>
-            <button className={"top-l md:w-15 lg:text-4xl flex justify-center text-2xl w-fit cursor-pointer hover:opacity-70 mr-2 text-indigo-600"}>
+
+            
+                <div className={"p-1 h-full mr-2 flex w-full justify-end"}>
+                    <input type={"search"} className={`md:w-full h-full transition-all ease-linear focus:outline-0 border-gray-400 border rounded-lg ${search? "w-2/3 opacity-100" : "w-0 pointer-events-none opacity-0" }`} placeholder={"Start typing to search"}/>
+                </div>
+            
+            <button className={`top-l md:w-15 lg:text-4xl flex justify-center text-2xl w-fit cursor-pointer hover:opacity-70 mr-2 text-indigo-600 `} onClick={()=>{
+                setsearch(!search)
+            }}>
                 <CiSearch />
             </button>
             <Link href={"/"} className="top-l md:w-15 lg:text-4xl flex justify-center text-4xl w-fit cursor-pointer hover:opacity-70 mr-2 text-indigo-600" >
